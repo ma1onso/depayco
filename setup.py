db1120253dc8974d87ce23d548d5f95f0222520f
Alonso Enrique <alons45@gmail.com>
2020-01-01 20:35:07 -0500

    add more dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
     name='depayco',
-    version='1.0',
+    version='1.0.1',
     license='GPL',
     author='Alonso',
     author_email='alons45@gmail.com',
@@ -14,11 +14,16 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/ma1onso/depayco',
     packages=setuptools.find_packages(),
-    install_requires=['pyepayco==1.0.0'],
+    install_requires=[
+        'pyepayco==1.0.0',
+        # TODO: don't repeat dependencies from pyepayco
+        'requests >= 2.4.3',
+        'pycrypto >= 2.3'
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
 )
