b7e7ee29ec331474a2b96fbacbebefce9488b08c
Alonso Enrique <alons45@gmail.com>
2020-01-26 12:04:24 -0500

    improvement log for sync_plans command

--- depayco/management/commands/sync_plans.py
+++ depayco/management/commands/sync_plans.py
@@ -15,15 +15,24 @@ class Command(BaseCommand):
 
                     if 'message' in response and response['message'] == 'Plan no encontrado':
                         plans.create(plan_data=settings_plan)
+
+                        self.stdout.write(self.style.SUCCESS(
+                            f'Successfully create plan {settings_plan["id_plan"]} on epayco and local DB'
+                        ))
+
                     elif response['success'] and not Plan.objects.filter(id=settings_plan['id_plan']).exists():
                         plans.sync_plan_from_epayco_data(epayco_plan_data=settings_plan)
+
+                        self.stdout.write(self.style.SUCCESS(
+                            f'Successfully create plan {settings_plan["id_plan"]} on local DB (from existing data on epayco)'
+                        ))
+
                     elif response['success']:
                         plans.update(plan_id=settings_plan['id_plan'], plan_data=settings_plan)
 
-                    self.stdout.write(self.style.SUCCESS(
-                        'Successfully create or update plan "%s" on epayco and local DB' %
-                        settings_plan['id_plan']
-                    ))
+                        self.stdout.write(self.style.SUCCESS(
+                            f'Successfully update plan {settings_plan["id_plan"]} on epayco and local DB'
+                        ))
 
         except AttributeError:
             self.stdout.write(self.style.WARNING('Is needed configure PLANS in settings.py'))
